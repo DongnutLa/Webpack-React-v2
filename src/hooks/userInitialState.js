445224f0e2e54f90ccd44c2cc9953ea7dc710269
DongnutLa <luahernandezdu@unal.edu.co>
2022-03-03 22:28:15 -0500

    Added backend

--- src/hooks/userInitialState.js
+++ src/hooks/userInitialState.js
@@ -1,17 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const API = 'https://us-central1-gndx-fake-api.cloudfunctions.net/api';
+const API = 'https://express-webpack-dongnut.herokuapp.com/api/v1';
 
 const useInitialState = () => {
-	const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-	useEffect(async () => {
-		const response = await axios(API);
-		setProducts(response.data);
-	}, []);
+  useEffect(async () => {
+    const response = await axios(API);
+    setProducts(response.data);
+  }, []);
 
-	return { products };
-}
+  return { products };
+};
 
 export default useInitialState;
